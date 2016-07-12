1f7db03a7a4111b0d51092e36e5aa16bc44ac8a1
Jonathan Needle <zxjon22@users.noreply.github.com>
2016-07-12 18:54:03 +0000

    Additional AssemblyInformationalVersion to GlobalAssemblyInfo.cs for nuget semver packaging

--- src/VelocityMail/GlobalAssemblyInfo.cs
+++ src/VelocityMail/GlobalAssemblyInfo.cs
@@ -18,3 +18,4 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-alpha")]
