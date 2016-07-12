98bde68baf90fc2b81f5c0bde8ad974826c9db19
Jonathan Needle <zxjon22@users.noreply.github.com>
2016-07-12 19:43:25 +0000

    Use GlobalAssemblyInfo.cs for versioning. Version is set by TC.

--- src/VelocityMail/Properties/AssemblyInfo.cs
+++ src/VelocityMail/Properties/AssemblyInfo.cs
@@ -2,13 +2,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("VelocityMail")]
-[assembly: AssemblyDescription("Apache Velocity-formatted e-mail templating service")]
-[assembly: AssemblyProduct("VelocityMail")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Jonathan Needle")]
 
 #if !PORTABLE && !PORTABLE40
 [assembly: ComVisible(false)]
