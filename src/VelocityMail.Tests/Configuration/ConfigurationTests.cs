2972f8a542b0efb1bfeefeb8f914c03d2e737177
Jonathan Needle <zxjon22@users.noreply.github.com>
2016-08-08 22:10:31 +0000

    Unit tests for different modes.

--- src/VelocityMail.Tests/Configuration/ConfigurationTests.cs
+++ src/VelocityMail.Tests/Configuration/ConfigurationTests.cs
@@ -18,6 +18,10 @@ namespace VelocityMail.Tests.Configuration
         /// </summary>
         private const string GLOBAL_VARS_TEMPLATE_BODY = "siteUrl=http://localhost:1234/, author=Jonathan";
 
+        /// <summary>
+        /// Body of the SimpleEmbedded-txt.vm template
+        /// </summary>
+        private const string SIMPLE_EMBEDDED_BODY = "Simple embedded template";
         #endregion
 
         /// <summary>
@@ -38,6 +42,46 @@ namespace VelocityMail.Tests.Configuration
 
             this.smtpServer.ReceivedEmailCount.Should().Be(1);
             this.smtpServer.ReceivedEmail[0].MessageParts[0].GetDecodedBody().Should().Be(GLOBAL_VARS_TEMPLATE_BODY);
+        }
+
+        /// <summary>
+        /// Test mode should prepend '[TEST]: ' to all subjects, but still send the e-mails
+        /// </summary>
+        [Fact]
+        public void TestMode()
+        {
+            var settings = this.GetTestConfig(@"Configuration\TestMode.config");
+            var svc = new VelocityMailService(settings);
+            var subject = "TestMode test";
+            svc.SmtpClientFactory = () => this.CreateSmtpClient();
+
+            using (var msg = svc.CreateMailMessage("SimpleEmbedded", "testfrom@test.com", "testto@test.com"))
+            {
+                msg.Subject = subject;
+                svc.Send(msg);
+            }
+
+            this.smtpServer.ReceivedEmailCount.Should().Be(1);
+            this.smtpServer.ReceivedEmail[0].MessageParts[0].GetDecodedBody().Should().Be(SIMPLE_EMBEDDED_BODY);
+            this.smtpServer.ReceivedEmail[0].Headers["Subject"].Should().Be("[TEST]: " + subject);
+        }
+
+        /// <summary>
+        /// Disabled mode should not send any e-mails.
+        /// </summary>
+        [Fact]
+        public void DisabledMode()
+        {
+            var settings = this.GetTestConfig(@"Configuration\DisabledMode.config");
+            var svc = new VelocityMailService(settings);
+            svc.SmtpClientFactory = () => this.CreateSmtpClient();
+
+            using (var msg = svc.CreateMailMessage("SimpleEmbedded", "testfrom@test.com", "testto@test.com"))
+            {
+                svc.Send(msg);
+            }
+
+            this.smtpServer.ReceivedEmailCount.Should().Be(0);
         }
 
         /// <summary>
