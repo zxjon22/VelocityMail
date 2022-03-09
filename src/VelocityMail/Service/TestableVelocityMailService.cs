6f589a6885dbdb63e14667f600d27be14257d78f
zxjon22 <jonathan.needle@gmail.com>
2022-03-09 13:23:12 +0000

    Add TestableVelocityMailService for easy unit testing.

---
+++ src/VelocityMail/Service/TestableVelocityMailService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace VelocityMail.Service
+{
+    /// <summary>
+    /// A testable implementation of <see cref="VelocityMailService"/> that captures e-mails
+    /// to <see cref="SentMessages"/> rather than actually sending them.
+    /// </summary>
+    public class TestableVelocityMailService : VelocityMailService
+    {
+        /// <inheritdoc/>
+        public TestableVelocityMailService(VelocityMailOptions options) : base(options)
+        {
+        }
+
+        private readonly List<MailMessage> _sentMessages = new List<MailMessage>();
+
+        /// <summary>
+        /// Gets the collection of sent <see cref="VelocityMailMessage"/> messages.
+        /// </summary>
+        public IReadOnlyCollection<MailMessage> SentMessages { get => _sentMessages; }
+
+        /// <summary>
+        /// Clears (deletes) all sent messages.
+        /// </summary>
+        public void ClearMessages()
+        {
+            _sentMessages.Clear();
+        }
+
+        /// <inheritdoc/>
+        public override void Send(VelocityMailMessage msg)
+        {
+            var mailMessage = msg.GetMailMessage(Engine, true);
+            _sentMessages.Add(mailMessage);
+        }
+    }
+}
