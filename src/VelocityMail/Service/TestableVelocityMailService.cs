c9d8da3629f26f238f0ee5656743f369716ca276
zxjon22 <jonathan.needle@gmail.com>
2022-05-03 14:01:41 +0100

    Enhance TestableVelocityMailService. Raw (unprocessed) VelocityMailMessage messages are stored in addition to the generated MailMessages. The class is now thread safe, supporting concurrent operations.

--- src/VelocityMail/Service/TestableVelocityMailService.cs
+++ src/VelocityMail/Service/TestableVelocityMailService.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Net.Mail;
 
 namespace VelocityMail.Service
 {
     /// <summary>
-    /// A testable implementation of <see cref="VelocityMailService"/> that captures e-mails
-    /// to <see cref="SentMessages"/> rather than actually sending them.
+    /// A testable thread-safe implementation of <see cref="VelocityMailService"/> that captures e-mails
+    /// to <see cref="SentMessages"/> and <see cref="RawMessages"/> rather than actually sending them.
     /// </summary>
     public class TestableVelocityMailService : VelocityMailService
     {
@@ -14,26 +15,36 @@ namespace VelocityMail.Service
         {
         }
 
-        private readonly List<MailMessage> _sentMessages = new List<MailMessage>();
+        private readonly ConcurrentQueue<MailMessage> _sentMessages = new ConcurrentQueue<MailMessage>();
+        private readonly ConcurrentQueue<VelocityMailMessage> _rawMessages = new ConcurrentQueue<VelocityMailMessage>();
 
         /// <summary>
-        /// Gets the collection of sent <see cref="VelocityMailMessage"/> messages.
+        /// Gets the collection of sent <see cref="MailMessage"/> messages.
         /// </summary>
-        public IReadOnlyCollection<MailMessage> SentMessages { get => _sentMessages; }
+        public MailMessage[] SentMessages { get => _sentMessages.ToArray(); }
+
+        /// <summary>
+        /// Gets the collection of sent <see cref="VelocityMailMessage"/> messages before
+        /// processing and converting to a <see cref="MailMessage"/>.
+        /// </summary>
+        public VelocityMailMessage[] RawMessages { get => _rawMessages.ToArray(); }
 
         /// <summary>
         /// Clears (deletes) all sent messages.
         /// </summary>
         public void ClearMessages()
         {
-            _sentMessages.Clear();
+            // NOTE: .NetStandard 2.0 does not contain `Clear()`. That first appears in 2.1.
+            while (_rawMessages.TryDequeue(out _)) { };
+            while (_sentMessages.TryDequeue(out _)) { };
         }
 
         /// <inheritdoc/>
         public override void Send(VelocityMailMessage msg)
         {
             var mailMessage = msg.GetMailMessage(Engine, true);
-            _sentMessages.Add(mailMessage);
+            _sentMessages.Enqueue(mailMessage);
+            _rawMessages.Enqueue(msg);
         }
     }
 }
