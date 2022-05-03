e29e9dd46f9b2c9615fc684cae10878621c9bfd0
zxjon22 <jonathan.needle@gmail.com>
2022-05-03 14:01:41 +0100

    Enhance TestableVelocityMailService. Raw (unprocessed) VelocityMailMessage messages are stored in addition to the generated MailMessages. The class is now thread safe, supporting concurrent operations.

---
+++ src/VelocityMail.Tests/TestableVelocityMailServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using System.Linq;
+using VelocityMail.Service;
+using Xunit;
+
+namespace VelocityMail.Tests
+{
+    /// <summary>
+    /// Tests for the <see cref="TestableVelocityMailService"/>.
+    /// </summary>
+    public class TestableVelocityMailServiceTests
+    {
+        public TestableVelocityMailServiceTests()
+        {
+        }
+
+        /// <summary>
+        /// Test capturing sent messages.
+        /// </summary>
+        [Fact]
+        public void Send()
+        {
+            var service = new TestableVelocityMailService(new VelocityMailOptions
+            {
+                TemplatesAssembly = "VelocityMail.Tests.Assets.Embedded, VelocityMail.Tests",
+            });
+
+            using (var msg = service.CreateMailMessage("SimpleEmbedded", "testfrom@test.com", "testto@test.com"))
+            {
+                msg.Cc.Add("testcc@test.com");
+                msg.ReplyTo.Add("reply@test.com");
+                service.Send(msg);
+            }
+
+            service.SentMessages.Should().HaveCount(1);
+            service.RawMessages.Should().HaveCount(1);
+
+            service.SentMessages.First().CC[0].Should().Be("testcc@test.com");
+            service.RawMessages.First().Cc[0].Should().Be("testcc@test.com");
+        }
+
+        /// <summary>
+        /// Test clearing captured messages.
+        /// </summary>
+        [Fact]
+        public void Clear()
+        {
+            var service = new TestableVelocityMailService(new VelocityMailOptions
+            {
+                TemplatesAssembly = "VelocityMail.Tests.Assets.Embedded, VelocityMail.Tests",
+            });
+
+            using (var msg = service.CreateMailMessage("SimpleEmbedded", "testfrom@test.com", "testto@test.com"))
+            {
+                msg.Cc.Add("testcc@test.com");
+                msg.ReplyTo.Add("reply@test.com");
+                service.Send(msg);
+            }
+
+            service.SentMessages.Should().HaveCount(1);
+            service.RawMessages.Should().HaveCount(1);
+
+            service.ClearMessages();
+
+            service.SentMessages.Should().HaveCount(0);
+            service.RawMessages.Should().HaveCount(0);
+        }
+    }
+}
